4a55ec9e9aca5cac2917e9ed05e19d0b9f11ba22
midlneedle <midlneedle@gmail.com>
2025-12-23 21:47:17 +0300

    Initial Commit

---
+++ expirimental/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  expirimental
+//
+//  Created by Vladislav Ivanov on 23.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ expirimental/expirimentalApp.swift
@@ -0,0 +1,17 @@
+//
+//  expirimentalApp.swift
+//  expirimental
+//
+//  Created by Vladislav Ivanov on 23.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct expirimentalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
